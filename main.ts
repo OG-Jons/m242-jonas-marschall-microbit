d3fb5a3db2aa6135392580597a58ce6cb5526594
Jonas <56066318+OG-Jons@users.noreply.github.com>
2022-01-28 14:33:15 +0100

    measure temperature

--- main.ts
+++ main.ts
@@ -1,14 +1,5 @@
-function exercise4(x: number, y: number, delay: number): void {
-    led.plot(x, y);
-    basic.pause(delay);
-    led.unplot(x, y);
-    basic.pause(delay);
-}
-
 basic.forever(function () {
-    exercise4(0, 0, 1000);
-})
-
-basic.forever(function () {
-    exercise4(4, 4, 100);
+    const temperature: number = input.temperature();
+    basic.showNumber(temperature);
+    basic.pause(1000);
 })
