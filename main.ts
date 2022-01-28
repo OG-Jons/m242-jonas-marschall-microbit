0399c78b36c1970caab6f2e870b22b4b61593e35
Jonas <56066318+OG-Jons@users.noreply.github.com>
2022-01-28 14:29:56 +0100

    Exercise 3

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
-basic.forever(function () {
-	
+function exercise3 (name: string): void {
+    basic.showString(name)
+    basic.pause(500)
+}
+
+basic.forever(function () { 
+    exercise3('JONAS');
 })
