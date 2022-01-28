e73510ab91df929b945cb562851b69bf03acdfb2
Jonas <56066318+OG-Jons@users.noreply.github.com>
2022-01-28 14:36:09 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,3 @@
-basic.forever(function () {
-    const temperature: number = input.temperature();
-    basic.showNumber(temperature);
-    basic.pause(1000);
+input.onShake(() => {
+    basic.showNumber(randint(0, 10));
 })
