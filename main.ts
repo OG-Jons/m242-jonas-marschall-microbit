c493782b2a3a95b19525106465d01da632029edd
Jonas <56066318+OG-Jons@users.noreply.github.com>
2022-01-28 14:31:14 +0100

    Exercise 4

--- main.ts
+++ main.ts
@@ -1,8 +1,14 @@
-function exercise3 (name: string): void {
-    basic.showString(name)
-    basic.pause(500)
+function exercise4(x: number, y: number, delay: number): void {
+    led.plot(x, y);
+    basic.pause(delay);
+    led.unplot(x, y);
+    basic.pause(delay);
 }
 
-basic.forever(function () { 
-    exercise3('JONAS');
+basic.forever(function () {
+    exercise4(0, 0, 1000);
+})
+
+basic.forever(function () {
+    exercise4(4, 4, 100);
 })
